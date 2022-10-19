c86b6e8908cd11beaf8e5eafb63b1f4d04556d3f
Rajas Ajay Kulkarni <47221329+kulkarni-rajas@users.noreply.github.com>
2022-10-19 12:33:17 +0530

    [ADD] Day 1 solution file for mentorship

---
+++ Ideas/SandBox/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SandBox;
+Console.WriteLine("Welcome to Employee Management System");
+Console.WriteLine("Press 1 for TCS");
+Console.WriteLine("Press 2 for Wells Fargo");
+
+string input = Console.ReadLine();
+int option  = Convert.ToInt32(input);
+
+if(option == 1)
+{
+    Console.WriteLine("Enter Employee Name, designation, basic salary, DOJ");
+    string name = Console.ReadLine();
+    string desig = Console.ReadLine();
+    string basicSalary = Console.ReadLine();
+    string DOJ = Console.ReadLine();
+    double BasicSalary = Convert.ToDouble(basicSalary);
+    DateTime Doj = DateTime.Parse(DOJ);
+    TCS obj = new TCS(desig,BasicSalary,name,Doj);
+}
---
+++ Ideas/SandBox/TCS.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using SandBox;
+
+public class TCS : IGovtRules
+{
+    private int empID;
+    private string empName;
+    private string dept;
+    private string desg;
+    private double basicSalary;
+    private static int empIDStart = 10000000;
+    private double employeePf;
+    private double employerPf;
+    private double pensionFund;
+    private int casualLeave;
+    private int sickLeave;
+    private int privilageLeave;
+    private DateTime DOJ;
+    private float serviceCompleted;
+
+
+    public TCS(string desg, double basicSalary, string empName, DateTime DOJ)
+    {
+        empIDStart++;
+        this.desg = desg;
+        this.basicSalary = basicSalary;
+        this.empName = empName;
+        this.empID = empIDStart;
+        this.DOJ = DOJ;
+        DateTime zeroTime = new DateTime(1, 1, 1);
+        TimeSpan temp = (DateTime.Now - DOJ);
+        serviceCompleted = (zeroTime + temp).Year - 1;
+        Console.WriteLine("Details are: - ");
+        Console.WriteLine(LeaveDetails());
+        Console.WriteLine(EmployeePF(basicSalary));
+        Console.WriteLine(gratuityAmount(serviceCompleted, basicSalary));
+
+    }
+    public string LeaveDetails()
+    {
+        casualLeave = 1;
+        sickLeave = 12;
+        privilageLeave = 10;
+
+        return "Leaves set successfully";
+    }
+
+    public double EmployeePF(double basicSalary)
+    {
+        double totalPf = 0;
+        totalPf = 20.33 * basicSalary;
+        pensionFund = 0.367 * basicSalary;
+        return totalPf;
+    }
+
+    public double gratuityAmount(float serviceCompleted, double basicSalary)
+    {
+        double gratuity = 0.0;
+        if (serviceCompleted > 20)
+        {
+            gratuity = basicSalary * 3;
+        }
+        else if (serviceCompleted > 10)
+        {
+            gratuity = basicSalary * 2;
+        }
+        else if (serviceCompleted > 5)
+        {
+            gratuity = basicSalary;
+        }
+        return gratuity;
+    }
+}
